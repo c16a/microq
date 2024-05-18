f0ea8de27745cb497957da035a0a62dd7cadf5e4
Chaitanya Munukutla <chaitanya.m61292@gmail.com>
2024-05-18 18:52:28 +0530

    Rename variable
    Signed-off-by: Chaitanya Munukutla <chaitanya.m61292@gmail.com>

--- main.go
+++ main.go
@@ -35,16 +35,16 @@ func main() {
 }
 
 func ParseJsonFile[T any](path string) (*T, error) {
-	var config T
+	var c T
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&config)
+	err = decoder.Decode(&c)
 	if err != nil {
 		return nil, err
 	}
-	return &config, nil
+	return &c, nil
 }
